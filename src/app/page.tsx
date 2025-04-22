28de6fcc2fcf35fa572494bc7c09d9683207b540
AkashAkter <akash31akter@gmail.com>
2025-04-22 22:30:56 +0600

    DONE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Login from "@/components/Login";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-24">
-      <h1>LOADING......</h1>
+    <div>
+      <Login />
     </div>
   );
 }
